53661edf987a7da04b47ee8688dbf5a7786ada0d
Cody Frank <cody.frank30@gmail.com>
2019-03-24 17:46:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(phrase)
   array = phrase.split(" ")
-  reverse_array = array.collect { |w| puts w.reverse}
+  reverse_array = array.collect { |w| w.reverse}
+  puts reverse_array
 end
